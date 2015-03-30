beddd2a80dd528878abc5f194a47b0baa67c0499
prashant <prashant@10.0.0.37>
2015-03-30 11:29:02 -0700

    test1

---
+++ PollApplication/src/test/java/demo/PollApplicationTests.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PollApplication.class)
+@WebAppConfiguration
+public class PollApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
