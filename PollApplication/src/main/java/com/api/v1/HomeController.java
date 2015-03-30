15b73677380a86505a104df3b80c40ed1ca4504c
prashant <prashant@10.0.0.37>
2015-03-30 15:13:32 -0700

    controller

---
+++ PollApplication/src/main/java/com/api/v1/HomeController.java
@@ -0,0 +1,25 @@
+package com.api.v1;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	/*
+	 * Create Moderator
+	 * Resource: /moderators
+	 * Description: Add a moderator to the system.
+	 * 
+	 * Request: 
+	 * POST /moderators (with the following payload in the request body)
+	 * 
+	 * HTTP Headers:
+	 * Content-type: application/json
+	 * 
+	 * Response:
+	 * HTTP Code: 201
+	 * 
+	 */
+	
+
+}
