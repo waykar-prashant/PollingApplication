1e8176188864f26caeee94d019e22355fd98ef08
prashant <prashant@10.0.0.37>
2015-03-30 15:13:54 -0700

    update

--- PollApplication/src/main/java/com/api/v1/Moderator.java
+++ PollApplication/src/main/java/com/api/v1/Moderator.java
@@ -20,4 +20,74 @@ public class Moderator {
 	private String password;
 	
 	private String createdAt;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the createdAt
+	 */
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	/**
+	 * @param createdAt the createdAt to set
+	 */
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
 }
--- PollApplication/src/main/java/com/api/v1/Poll.java
+++ PollApplication/src/main/java/com/api/v1/Poll.java
@@ -1,5 +1,84 @@
 package com.api.v1;
 
 public class Poll {
-
+	private String id;
+	private String question;
+	private String startedAt;
+	private String expiredAt;
+	private String[] choice;
+	private int[] results;
+	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the question
+	 */
+	public String getQuestion() {
+		return question;
+	}
+	/**
+	 * @param question the question to set
+	 */
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	/**
+	 * @return the startedAt
+	 */
+	public String getStartedAt() {
+		return startedAt;
+	}
+	/**
+	 * @param startedAt the startedAt to set
+	 */
+	public void setStartedAt(String startedAt) {
+		this.startedAt = startedAt;
+	}
+	/**
+	 * @return the expiredAt
+	 */
+	public String getExpiredAt() {
+		return expiredAt;
+	}
+	/**
+	 * @param expiredAt the expiredAt to set
+	 */
+	public void setExpiredAt(String expiredAt) {
+		this.expiredAt = expiredAt;
+	}
+	/**
+	 * @return the choice
+	 */
+	public String[] getChoice() {
+		return choice;
+	}
+	/**
+	 * @param choice the choice to set
+	 */
+	public void setChoice(String[] choice) {
+		this.choice = choice;
+	}
+	/**
+	 * @return the results
+	 */
+	public int[] getResults() {
+		return results;
+	}
+	/**
+	 * @param results the results to set
+	 */
+	public void setResults(int[] results) {
+		this.results = results;
+	}
+	
 }
