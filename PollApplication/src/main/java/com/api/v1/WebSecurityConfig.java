97b4c9f725444746029f520183014c39b482ca95
waykar-prashant <waykar.prashant@gmail.com>
2015-04-06 04:50:49 -0700

    added security

---
+++ PollApplication/src/main/java/com/api/v1/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.api.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@ComponentScan
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		 auth
+		.inMemoryAuthentication()
+		.withUser("foo").password("bar").roles("USER");
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/api/v1/moderators/*").hasRole("USER").and().httpBasic();
+		http.csrf().disable().authorizeRequests().antMatchers("/api/v1/moderators/*/*").hasRole("USER").and().httpBasic();
+		http.csrf().disable().authorizeRequests().antMatchers("/api/v1/moderators/*/polls/*").hasRole("USER").and().httpBasic();
+	}
+}	
+
